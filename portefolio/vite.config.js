3cc77ca0e180570c20cb0cde91f184bfa930b9de
Ophélie BELLISSENS <obellissens1@gmail.com>
2026-06-14 12:40:00 +0200

    feat: ajouter la configuration du plugin Sitemap dans vite.config.js

--- portefolio/vite.config.js
+++ portefolio/vite.config.js
@@ -3,16 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Sitemap({
+      // METTEZ VOTRE VRAIE ADRESSE NETLIFY ICI :
+      hostname: 'https://opheliebellissens.netlify.app', 
+      dynamicRoutes: [
+        '/',
+        // Ajoutez ici les autres pages de votre site si vous en avez (ex: '/contact', '/a-propos')
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
